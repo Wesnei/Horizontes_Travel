1ef8499a36f0d7e08aca9397ee83c5871df6c476
Wesnei <wesneipaiva@gmail.com>
2024-10-17 01:14:07 -0300

    Inserção das telas de Destino, Reserva e Dados. Além da função onClick para quando clicar em Cadastrar pacote ir para página de pacote turístico

--- src/main/java/com/example/agencia_viagensfx/HelloController.java
+++ src/main/java/com/example/agencia_viagensfx/HelloController.java
@@ -7,18 +7,37 @@ import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
-
 import java.io.IOException;
-
 
 public class HelloController {
 
     @FXML
     protected void switchToCadastroPessoa(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("cadastroPessoa.fxml"));  // Carregue a página de Cadastro Pessoa
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/agencia_viagensfx/cadastroPessoa.fxml"));
+        Stage newStage = new Stage();
+        Scene scene = new Scene(root);
+        newStage.setScene(scene);
+        newStage.show();
+    }
+
+    @FXML
+    protected void switchPacoteTuristico(ActionEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/agencia_viagensfx/PacoteTuristico.fxml"));
+        Stage newStage = new Stage();
+        Scene scene = new Scene(root);
+        newStage.setScene(scene);
+        newStage.show();
+    }
+
+    @FXML
+    protected void switchDestino(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/agencia_viagensfx/Destino.fxml"));
         Stage newStage = new Stage();
         Scene scene = new Scene(root);
         newStage.setScene(scene);
         newStage.show();
     }
 }
+
+
